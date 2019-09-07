527ce3f977ece29c9426f5c2d203ba196132c55b
dannertech <danner@danner.tech>
2019-09-06 22:32:26 -0400

    successfully render plane

--- MyiOSPet/ViewController.swift
+++ MyiOSPet/ViewController.swift
@@ -11,6 +11,7 @@ import ARKit
 import SceneKit
 
 class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    
     @IBOutlet var sceneView: ARSCNView!
     @IBAction func addPet(_ sender: Any) {
     }
@@ -29,6 +30,31 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(true)
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        //get size of anchor
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+        
+        
+        plane.materials.first?.diffuse.contents = UIColor.green
+        //create a plane
+        let planeNode = SCNNode(geometry: plane)
+        
+        
+        //get position of anchor
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        //set position
+        planeNode.position = SCNVector3(x, y, z)
+        planeNode.eulerAngles.x = -.pi/2
+        
+        node.addChildNode(planeNode)
     }
 
 
